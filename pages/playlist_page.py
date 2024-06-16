7bc5d1325987e63ee957ccb4c120d52664beca65
Ivan Lemeshkin <106601642+LemeshkinIvan@users.noreply.github.com>
2024-06-16 19:09:39 +0300

    Add files via upload

---
+++ pages/playlist_page.py
@@ -0,0 +1,19 @@
+import tkinter
+from tkinter import ttk
+from typing import Any
+import config
+
+
+class PlaylistPage:
+    frame: tkinter.Frame = None
+
+    def __init__(self):
+        self.frame["height"] = config.HEIGHT
+        self.frame["width"] = config.WIDTH
+
+    def init_tree_table(self, frame, columns: list[str],
+                        columns_settings: dict[str, Any]):
+        tree = ttk.Treeview(frame, columns=columns, show="headings")
+        tree["height"] = 5
+        tree.grid(row=1, column=0)
+
