010eb15bfe7780be4c5e73b70b53a780a8ee25a8
Ivan Lemeshkin <106601642+LemeshkinIvan@users.noreply.github.com>
2024-06-16 19:01:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 WIDTH = 1200
 HEIGHT = 700
-icon = "source/image/icon.ico"
+
+ICON_APP = "source/image/app.ico"
+ICON_LOGIN = "source/image/hqdefault.png"
+
 # maybe style settings and other stuff
+FRAME_COLOR = "#26272a"
